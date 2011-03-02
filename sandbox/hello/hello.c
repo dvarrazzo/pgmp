cd654fb4f9dc5e58703951ade47cc21ceff3a76d
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-03-02 00:59:17 +0000

    Added toy in the sandbox to peek at the structures

---
+++ sandbox/hello/hello.c
@@ -0,0 +1,22 @@
+/* A test program to study the mpz_t structure.
+ *
+ * Copyright (C) 2011 Daniele Varrazzo
+ */
+
+#include <stdio.h>
+#include <gmp.h>
+
+int
+main(int argc, char **argv)
+{
+    mpz_t z1, z2;
+
+    mpz_init_set_ui(z1, ~((unsigned long int)0));
+    mpz_init(z2);
+    mpz_add_ui(z2, z1, 1);
+
+    mpz_out_str(stdout, 10, z2);
+    printf("\n");
+
+    return 0;
+}
