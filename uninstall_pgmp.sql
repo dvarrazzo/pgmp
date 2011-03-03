1822913c6fd89e0929e702360937c80d92ff5f41
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-03-03 00:42:37 +0000

    Added uninstall script

---
+++ uninstall_pgmp.sql
@@ -0,0 +1,26 @@
+/* pgmp -- Module uninstall SQL script
+ *
+ * Copyright (C) 2011 Daniele Varrazzo
+ *
+ * This file is part of the PostgreSQL GMP Module
+ *
+ * The PostgreSQL GMP Module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * The PostgreSQL GMP Module is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the PostgreSQL GMP Module.  If not, see
+ * http://www.gnu.org/licenses/.
+ */
+
+-- Adjust this setting to control where the objects get created.
+SET search_path = public;
+
+DROP TYPE mpz CASCADE;
+
