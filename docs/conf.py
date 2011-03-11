75fe215503bc5a2e33fa44cddd0c8fd53e85c6cc
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-03-11 09:52:27 +0000

    Using Sphinx 1.1
    This allows the use of the MathJax plugin, so we don't need all the fonts.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ import sys, os
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
-    'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
+    'sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
